a6f10a44e214fbebb01c108ea75e83fcef359cdc
Sabu <sabuw319@gmail.com>
2018-09-17 20:41:54 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -6,9 +6,9 @@ function forLoop(array){
    
    if(i === 1){
      array.push("I am 1 strange loop")
-   } else if(i === 0){
+   }  /* else if(i === 0){
      array.push(`I am ${i} strange loops`)
-   } else { 
+   } */  else { 
      array.push(`I am ${i} strange loops`)
    }
   return array
