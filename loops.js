d2c18da0b3f84a1902c2ab23b7c1dcfff09d1120
Sabu <sabuw319@gmail.com>
2018-09-17 18:02:05 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,19 @@
+function forLoop(array){
+  
+  for(var i = 0; i < 25; i++){
+   array.push("I am `${i}` strange loop")
+   }
+  return array
+}
+
+
+
+function doWhileLoop(array){
+  do{
+  array.length--
+  } while {
+  array.length === i && i < 0
+    }
+  }
+ 
+
